ba4f10d3da2723b907c718e69643d66ecf012d7b
nbgao <nbgao@126.com>
2019-11-29 19:51:55 +0800

    Rewrite W6-P1

--- W6/P1.cpp
+++ W6/P1.cpp
@@ -4,51 +4,55 @@
 using namespace std;
 
 int n;
-int P[N][N], Q[N][N], maxout[N];
+int P[N][N], Q[N][N];
 
 struct S{
-    int id;
-    int sum;
-    int Max;
-    int up;
+    int id, sum, Max;
     int *x;
 };
 
+int Compute(int k, int *x){
+    int s = 0;
+    for(int i=1;i<=k;i++)
+        s += P[i][x[i]]*Q[x[i]][i];
+    return s;
+}
+
 struct cmp{
     bool operator()(S a, S b){
-        return a.up < b.up;
+        return a.Max < b.Max;
     }
 };
 
 int BFS(){
+    int Max = 0;
     priority_queue<S, vector<S>, cmp> q;
-    S s = S{1,0,0,0};
-    for(int i=1;i<=n;i++)
-        s.Max += maxout[i];
-    s.up = s.Max;
+    S s = S{1,0,0};
     s.x = new int[n+1];
     for(int i=1;i<=n;i++)
         s.x[i] = i;
-
+    
     q.push(s);
     while(!q.empty()){
         S p = q.top();
         q.pop();
-        if(p.id > n)
-            return p.sum;
-            
-        for(int i=p.id;i<=n;i++){
+        if(p.id == n){
+            int t = Compute(n, p.x);
+            if(t>Max)
+                Max = t;
+            return Max;
+        }
+        for(int i=p.id+1;i<=n;i++){
             S r;
-            r.id = p.id+1;
             r.x = new int[n+1];
+            r.id = p.id + 1;
+            r.Max = p.Max;
             for(int j=1;j<=n;j++)
                 r.x[j] = p.x[j];
-            int u = p.id;
+            int u = r.id;
             r.x[u] = p.x[i];
             r.x[i] = p.x[u];
-            r.sum = p.sum + P[u][r.x[u]]*Q[r.x[u]][u];
-            r.Max = p.Max - maxout[u];
-            r.up = r.sum + r.Max;
+            r.Max = Compute(r.id , r.x);
             q.push(r);
         }
     }
@@ -57,7 +61,7 @@ int BFS(){
 // x[i]表示第i个男运动员与配对的女运动员号,r表示最佳配对列表，Max表示最佳竞赛优势总和
 int main(){
     cin>>n;
-    int x[n], r[n], Max=0;
+    int x[n], Max=0;
     for(int i=1;i<=n;i++)   //初始化配对
         x[i] = i;
     for(int i=1;i<=n;i++)
@@ -67,12 +71,6 @@ int main(){
         for(int j=1;j<=n;j++)
             cin>>Q[i][j];
     
-    for(int i=1;i<=n;i++){
-        int t = 0;
-        for(int j=1;j<=n;j++)
-            t = max(t, P[i][j]*Q[j][i]);
-        maxout[i] = t;
-    }
     cout<<BFS()<<endl;
 
     return 0;
